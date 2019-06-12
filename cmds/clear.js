c88233571723ae284a426b663d443f85b813d39b
Mad-Gamer <51133545+Mad-Gamer@users.noreply.github.com>
2019-06-12 16:57:34 +0100

    clear.js
    mis en anglais

--- cmds/clear.js
+++ cmds/clear.js
@@ -6,7 +6,7 @@ function clear(message, client, prefix, args) {
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("nop.");
 if(!args[0]) return message.channel.send("try again");
 message.channel.bulkDelete(args[0]).then(() => {
-message.channel.send(`**Vous avez effacer ${args[0]} messages.**`).then(msg => msg.delete(5000));
+message.channel.send(`**You have cleared ${args[0]} messages.**`).then(msg => msg.delete(5000));
     });
 }
 }

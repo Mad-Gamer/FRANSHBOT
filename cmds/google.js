c27c42bbcae2e201bf577c4703dd9bb953555359
Mad-Gamer <51133545+Mad-Gamer@users.noreply.github.com>
2019-06-12 17:00:42 +0100

    google.js
    mis en anglais

--- cmds/google.js
+++ cmds/google.js
@@ -9,10 +9,10 @@ function google(message, client, prefix) {
         let embed = new Discord.RichEmbed()
         embed.setColor("#00ff00")
             .setTimestamp()
-            .addField("Voila votre recherche", `Recheche [Google]: \n [${args.join(" ")}](https://www.google.fr/#q=${args.join("%20")}&btnI=I%27m+Feeling+Lucky)`)
+            .addField("Here is your search", `Recheche [Google]: \n [${args.join(" ")}](https://www.google.fr/#q=${args.join("%20")}&btnI=I%27m+Feeling+Lucky)`)
             .setThumbnail("http://www.slate.com/content/dam/slate/blogs/future_tense/2015/09/01/google_s_new_animated_logo_is_the_future_of_mobile_branding/google_logo.gif.CROP.original-original.gif")
-            .setFooter("Recherche Google ", "http://www.slate.com/content/dam/slate/blogs/future_tense/2015/09/01/google_s_new_animated_logo_is_the_future_of_mobile_branding/google_logo.gif.CROP.original-original.gif")
-            .setFooter("MadGamer : plein de nouveaux trucs cool vont arrivé 😜! | ", `${client.user.displayAvatarURL}`)
+            .setFooter("Google Search ", "http://www.slate.com/content/dam/slate/blogs/future_tense/2015/09/01/google_s_new_animated_logo_is_the_future_of_mobile_branding/google_logo.gif.CROP.original-original.gif")
+            .setFooter("MadGamer : lots of new cool stuff will come 😜! | ", `${client.user.displayAvatarURL}`)
         message.channel.send({ embed: embed });
 
     }

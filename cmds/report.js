80d1cbc84e8d27639e2bc1627527b9f1b899c638
Mad-Gamer <51133545+Mad-Gamer@users.noreply.github.com>
2019-06-12 17:07:49 +0100

    report.js
    mis en anglais

--- cmds/report.js
+++ cmds/report.js
@@ -3,21 +3,21 @@ const Discord = require('discord.js');
 function report(message,client,prefix, args){
     if (message.content.startsWith(prefix+'report')){
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!rUser) return message.channel.send("Cet utilisateur n'existe pas !");
+    if(!rUser) return message.channel.send("This user does not exist !");
     let reason = args.join(" ").slice(22);
 
     let reportembed = new Discord.RichEmbed()
     .setColor("04B201")
-    .setTitle("Signalé :")
-    .addField("Utilisateur signalé", `${rUser} | ***ID***: ${rUser.id}`)
-    .addField("Signalé par", `${message.author} | ***ID***: ${message.author.id}`)
-    .addField("Salon", message.channel)
-    .addField("Heure et date du signalement", message.createdAt)
-    .addField("Raison du signalement", reason)
-    .setFooter("Créé par MadGamer avec l'aide de MPZ")
+    .setTitle("reported :")
+    .addField("user reported", `${rUser} | ***ID***: ${rUser.id}`)
+    .addField("reported by", `${message.author} | ***ID***: ${message.author.id}`)
+    .addField("Living room", message.channel)
+    .addField("Time and date of report", message.createdAt)
+    .addField("Reason for reporting", reason)
+    .setFooter("Created by MadGamer with the help of MPZ")
     .setTimestamp();
 
-    message.channel.send("Ton rapport a etait envoyer, MERCI");
+    message.channel.send("Your report was sent, THANK YOU");
 
     client.guilds.get('547534390666985487').channels.get('585468050900516864').send(reportembed)
     

ce5d18de710cba3e3680fecaf3fa28d407974491
Mad-Gamer <51133545+Mad-Gamer@users.noreply.github.com>
2019-06-12 17:02:49 +0100

    invites.js
    mis en anglais

--- cmds/invites.js
+++ cmds/invites.js
@@ -5,14 +5,14 @@ if(message.content === prefix + "invite"){
     let embed = new Discord.RichEmbed()
     embed
         .setColor("RANDOM")
-        .setTitle("Voici le lien pour inviter /FRENCHBOT/")
-        .addField("Lien :", "https://discordapp.com/api/oauth2/authorize?client_id=580484061504602122&permissions=1879571575&scope=bot")
+        .setTitle("Here is the link to invite /FRENCHBOT/")
+        .addField("Link :", "https://discordapp.com/api/oauth2/authorize?client_id=580484061504602122&permissions=1879571575&scope=bot")
         // .addField("Message du développeur :", "LE MESSAGE")
     message.author.send(embed)
         let invitesembed = new Discord.RichEmbed()
         invitesembed
             .setColor("RANDOM")
-            .setTitle("regarde tes DM.")
+            .setTitle("look at your DM.")
 
             message.reply(invitesembed);
     }

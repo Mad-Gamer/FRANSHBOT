ec4d3fa8e1062cc453b3dd0fbc90fa2f667e771e
Mad-Gamer <51133545+Mad-Gamer@users.noreply.github.com>
2019-06-12 16:56:17 +0100

    bug.js
    mis en anglais

--- cmds/bugs.js
+++ cmds/bugs.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 function bug(message, client, prefix) {
 
     if (message.content.startsWith(prefix + 'bugs')) {
-        message.channel.send('Le BOT bug ? alors clique sur le lien pour expliquer : https://github.com/Mad-Gamer/FRANSHBOT/issues/created_by/Mad-Gamer');
+        message.channel.send('Le BOT bug ? then click on the link to explain : https://github.com/Mad-Gamer/FRANSHBOT/issues/created_by/Mad-Gamer');
     }
 }
 

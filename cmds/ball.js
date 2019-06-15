fd653c79981fae98f5519af24e48e46f00204b31
Mad-Gamer <51133545+Mad-Gamer@users.noreply.github.com>
2019-06-15 16:13:32 +0100

    ball.js
    mis en anglais

--- cmds/ball.js
+++ cmds/ball.js
@@ -4,37 +4,34 @@ function ball(message, client, prefix){
 
     if(message.content.startsWith(prefix + "8ball") || message.content.startsWith(prefix + '8b')){
         const reponses = [
-            'Prochainement',
-            'Oui',
-            'Absolument!',
+            'shortly',
+            'yes',
+            'Absolutely!',
             'Jamais',
-            'Quand tu es prêt.',
-            'Espérons',
-            "Je suis un bot je ne peut pas vous repondre",
-            'N\'espérons pas',
-            'Pourquoi demandez-vous cela?',
-            'Quel genre de question est-ce?',
-            'Sur mon cadavre!',
-            'Haha, Bonne blague !',
-            'Tu es qui ?',
-            'Mais nimp',
-            'Serait tu un cousin de Lepen ??',
-            'Non',
-            "c'est drole ptdr",
+            'When you are ready.',
+            'hopefully',
+            "I am a bot I can not answer you",
+            'N\'hope not',
+            'Why are you asking this?',
+            'What kind of question is this?',
+            'On my corpse!',
+            'Haha, good joke !',
+            'Who are you ?',
+            'Would you be a cousin of Lepen ??',
+            'NO',
+            "it's funny ptdr",
             "01110010110000111010101010011",
-            "Erreur 404",
-            "Euuu FBI [Fausse bonne idée]",
+            "Fault 404",
             ". . .",
-            "JPP chui mort",
-            "ou sinon reflechis et t'auras peu etre la reponse ! *sinon c que t con*",
+            "JPP I am dead",
+            "or else think about it and you will be able to be the answer ! *if not what you are con*",
             "AH !",
-            "Nan jure ?! *M'en blc frr*",
-            "Tu es une sous race",
-            "Tu suce pour un Mars ?",
-            "Euuuh nique ta mère",
-            "T'es drôle toi -_-",
-            "Demande a ta mère",
-            "Ne rage pas mon enfant",
+            "You are a sub race",
+            "You suck for a Mars ?",
+            "Euuuh fuck your mother",
+            "You're funny, you -_-",
+            "Ask your mother",
+            "Do not rage my child",
             
             
         ];
@@ -47,9 +44,9 @@ function ball(message, client, prefix){
         let reponse = randomItem(reponses);
 
         if (args.length < 1) {
-            message.channel.send('**Pose moi une question bordel !**');
+            message.channel.send('**Ask me a question brothel !**');
         } else
-            message.channel.send("Voyons....").then((msg) => {
+            message.channel.send("Let's....").then((msg) => {
                 msg.edit(`:8ball: | **${reponse}**`);
             });
 }      

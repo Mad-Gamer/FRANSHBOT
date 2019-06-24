49e74892c340865ca81ef938c8ac6226565b3ca2
Mad-Gamer <51133545+Mad-Gamer@users.noreply.github.com>
2019-06-24 09:15:42 +0100

    counter.js
    compteur en fonctionnement

--- cmds/counter.js
+++ cmds/counter.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
+
+function milliseconds_until_WatchDogs_3(){
+    return Date.parse("Mars 6 2020 00:00:00 GMT+0200") - Date.parse(new Date());
+}
 function days_until_WatchDogs_3(){
-    var distance = milliseconds_until_WatchDpgs_3()
+    var distance = milliseconds_until_WatchDogs_3()
     var days = Math.floor( milliseconds_until_WatchDogs_3()/(1000*60*60*24) );
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     return `${days}d ${hours}h ${minutes}m ${seconds}s`;
 }
-function milliseconds_until_WatchDogs_3(){
-    return Date.parse("Mars 6 2020 00:00:00 GMT+0200") - Date.parse(new Date());
-}
-
 
 function WatchDogs_3_countdown(client) {
     const channel = client.channels.get('591620909987790879');

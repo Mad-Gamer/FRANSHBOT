a68a077ba8390b32ad50f4bd0f833e328c886bc9
Mad-Gamer <51133545+Mad-Gamer@users.noreply.github.com>
2019-06-28 18:45:21 +0100

    merci.js
    Merci à tous

--- cmds/merci.js
+++ cmds/merci.js
@@ -7,7 +7,7 @@ function merci(message, client, prefix) {
 
   .setColor("#01FFDD")
   .setTitle("MERCI A TOUS")
-  .addField("")
+  .addField("Je tien à remercier Greep / A French Koala (DVD) / et MPZ de m'avoir aid~r a fondé ce magnifique BOT et j'espère pouvoir vous aidez à modéré votre serveur et aussi à vous divertir avec mes commande fun et rigolote Je vous invite si vous le voulez à invité mon BOT dans votre serveur et à y contribuer ")
   .setFooter("By MadGamer")
 
   message.channel.send(link) // Reponse du bot

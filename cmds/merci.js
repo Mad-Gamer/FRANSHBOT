56f84187f1b47ef17a50e0403419dd2b4eed7554
Mad-Gamer <51133545+Mad-Gamer@users.noreply.github.com>
2019-07-01 05:37:01 +0100

    merci.js
    ok

--- cmds/merci.js
+++ cmds/merci.js
@@ -3,12 +3,12 @@ const Discord = require('discord.js');
 function merci(message, client, prefix) {
   if (message.content.startsWith(prefix + 'merci'))
 
-    let merci = new Discord.RichEmbed()
+  let embed = new Discord.RichEmbed()
 
-  .setColor("#01FFDD")
-  .setTitle("MERCI A TOUS")
-  .addField("Je tien à remercier Greep / A French Koala (DVD) / et MPZ de m'avoir aider a fondé ce magnifique BOT et j'espère pouvoir vous aidez à modéré votre serveur et aussi à vous divertir avec mes commande fun et rigolote Je vous invite si vous le voulez à invité mon BOT dans votre serveur et à y contribuer ")
-  .setFooter("By MadGamer")
+  embed.setColor("#01FFDD")
+        .setTitle("MERCI A TOUS")
+        .addField("Je tien à remercier Greep / A French Koala (DVD) / et MPZ de m'avoir aider a fondé ce magnifique BOT et j'espère pouvoir vous aidez à modéré votre serveur et aussi à vous divertir avec mes commande fun et rigolote Je vous invite si vous le voulez à invité mon BOT dans votre serveur et à y contribuer ")
+         .setFooter("By MadGamer")
 
   message.channel.send(merci) // Reponse du bot
     
